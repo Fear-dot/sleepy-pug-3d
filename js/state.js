237e002a48d2c6fa1cc76aa658477fb90f90308e
Fear-dot <manuelklingel@gmail.com>
2026-08-28 01:47:35 +0200

    Delete js directory

--- js/state.js
+++
@@ -1,8 +0,0 @@
-window.SleepingPets=window.SleepingPets||{};
-SleepingPets.createState=function(){return{
- bankCoins:Number(localStorage.getItem("sleepingPets.bankCoins")||0),
- runCoins:0,combo:0,multiplier:1,wake:0,timer:15,elapsed:0,paused:false,gameOver:false,
- lastFrame:performance.now(),spawnAccumulator:0,
- visualPetBand:0,targetPetBand:0,animatingPet:false,petAnimRAF:null
-}};
-SleepingPets.persistBank=s=>localStorage.setItem("sleepingPets.bankCoins",String(s.bankCoins));
